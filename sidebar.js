d7f0ace1d4b90f70c1b53cb0cf8f6bf0c567b48f
Vedant Singhania <vedant.singhania@gmail.com>
2021-04-05 09:20:29 -0500

    reverted to plain sidebar w/ dropdown

--- sidebar.js
+++
@@ -1,6 +0,0 @@
-function changeActive(id, arr) {
-    document.getElementById(id).className = "nav-link active"
-    for (i=0;i<arr.length;i++) {
-      document.getElementById(arr[i]).className = "nav-link"
-    }
-  }
